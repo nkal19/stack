2495aaa83dd01eb5126f8f81ad980f4b41a56715
N Kalaiya <nkalaiya@mdsol.com>
2013-11-27 10:50:25 +0000

    changed conf file for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,10 +27,14 @@ Stack::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  #config.assets.compile = false
+  #we changed this for heroku
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
-  config.assets.digest = true
+  #config.assets.digest = true
+  #for heroku we changed this line
+  config.assets.digest = false
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
